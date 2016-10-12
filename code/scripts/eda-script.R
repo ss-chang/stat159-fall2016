3f6f2a15fd2d15976f933e247eab3af1fec611f6
Shannon Chang <changxshannon@gmail.com>
2016-10-12 03:12:38 -0700

    Fixed output path files to comply with repository Makefile

--- code/scripts/eda-script.R
+++ code/scripts/eda-script.R
@@ -1,8 +1,7 @@
 # ==============================================================================
 # Read in Advertising.csv data set
 # ==============================================================================
-advertising <- read.csv(file = "code/data/Advertising.csv", 
-                        row.names = 1)
+advertising <- read.csv(file = "../data/Advertising.csv", row.names = 1)
 
 
 
@@ -10,15 +9,18 @@ advertising <- read.csv(file = "code/data/Advertising.csv",
 # Generating eda-output.txt
 # ==============================================================================
 # sink output
-sink(file = "data/eda-output.txt")
+sink(file = "../data/eda-output.txt")
 
 # Summary statistics
-print(noquote("Summary Statistics for Advertising.csv"))
-summary(advertising) 
+cat("Summary Statistics for Advertising.csv\n")
+adv_sum <-summary(advertising)
+print(adv_sum)
+cat("\n\n\n")
 
 # Matrix of correlations
-print(noquote("Matrix of Correlations Among All Variables"))
-cor(advertising, use = "all.obs")
+cat("Matrix of Correlations Among All Variables\n")
+adv_cor <- cor(advertising, use = "all.obs")
+print(adv_cor)
 
 # STOP
 sink()
@@ -26,35 +28,42 @@ sink()
 
 
 # ==============================================================================
+# Generating correlation-matrix.RData
+# ==============================================================================
+save(adv_cor, file = '../data/correlation-matrix.RData')
+
+
+# ==============================================================================
 # Generating images
 # ==============================================================================
 # HISTOGRAMS
 # TV
-png("images/histogram-tv.png")
-tv_hist <- hist(advertising$TV, 
+png("../images/histogram-tv.png")
+hist(advertising$TV, 
                 xlab = "$ (in thousands)", 
                 main = "TV Advertising Budgets",
                 ylim = c(0,50))
 dev.off()
 
 # Radio
-png("images/histogram-radio.png")
-radio_hist <- hist(advertising$Radio, 
+png("../images/histogram-radio.png")
+hist(advertising$Radio, 
                 xlab = "$ (in thousands)", 
                 main = "Radio Advertising Budgets")
+
 dev.off() 
 
 # Newspaper 
-png("images/histogram-newspaper.png")
-newspaper_hist <- hist(advertising$Newspaper, 
+png("../images/histogram-newspaper.png")
+hist(advertising$Newspaper, 
                        xlab = "$ (in thousands)", 
                        main = "Newspaper Advertising Budgets",
                        ylim = c(0,50))
 dev.off()
 
 # Sales
-png("images/histogram-sales.png")
-sales_hist <- hist(advertising$Sales, 
+png("../images/histogram-sales.png")
+hist(advertising$Sales, 
                    xlab = "Units of Product Sold (in thousands)",
                    main = "Sales",
                    ylim = c(0,80))
@@ -62,6 +71,6 @@ dev.off()
 
 
 # Scatterplot matrix
-png("images/scatterplot-matrix.png")
+png("../images/scatterplot-matrix.png")
 plot(advertising)
 dev.off()
