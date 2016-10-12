d72e915d73dacb247c0487f699c197c6d3d8c4fe
Shannon Chang <changxshannon@gmail.com>
2016-10-12 15:48:57 -0700

    Reorganized expectations to reduce clutter

--- lab07/test-that/tests/tests.R
+++ lab07/test-that/tests/tests.R
@@ -6,75 +6,40 @@ source("../functions/range-value.R")
 context("Test for range value") 
 
 
-test_that("range value for numeric vectors", {
+test_that("range value", {
   x <- c(1, 2, 3, 4, 5)
+  y <- c(1, 2, 3, 4, NA)
+  z <- c(TRUE, FALSE, TRUE)
+  w <- letters[1:5]
   
   expect_equal(range_value(x), 4)
   expect_length(range_value(x), 1)
   expect_type(range_value(x), 'double')
+  expect_equal(range_value(y), NA_real_)
+  expect_length(range_value(y), 1)
+  expect_equal(range_value(z), 1L)
+  expect_length(range_value(z), 1)
+  expect_type(range_value(z), 'integer')
+  expect_error(range_value(w))
 })
 
-test_that("range value for numeric vectors", {
+test_that("range value", {
   x <- c(1, 2, 3, 4, 5)
+  y <- c(1, 2, 3, 4, NA)
+  z <- c(TRUE, FALSE, TRUE)
+  w <- letters[1:5]
   
   expect_equal(range_value(x, na.rm = TRUE), 4)
   expect_length(range_value(x, na.rm = TRUE), 1)
   expect_type(range_value(x, na.rm = TRUE), 'double')
-})
-
-
-
-
-
-test_that("range value for numeric vectors with NAs", {
-  y <- c(1, 2, 3, 4, NA)
-  
-  expect_equal(range_value(y), NA_real_)
-  expect_length(range_value(y), 1)
-}) 
-
-test_that("range value for numeric vectors with NAs, na.rm = TRUE", {
-  y <- c(1, 2, 3, 4, NA)
-
   expect_equal(range_value(y, na.rm = TRUE), 3)
   expect_length(range_value(y, na.rm = TRUE), 1)
-  })
-
-
-
-
-
-test_that("range value for logical vectors", {
-  z <- c(TRUE, FALSE, TRUE)
-  
-  expect_equal(range_value(z), 1L)
-  expect_length(range_value(z), 1)
-  expect_type(range_value(z), 'integer')
-})
-
-test_that("range value for logical vectors", {
-  z <- c(TRUE, FALSE, TRUE)
-  
   expect_equal(range_value(z, na.rm = TRUE), 1L)
   expect_length(range_value(z, na.rm = TRUE), 1)
   expect_type(range_value(z, na.rm = TRUE), 'integer')
-})
-
-
-
-
-
-test_that("range value stops for character vectors", {
-  w <- letters[1:5]
-  
-  expect_error(range_value(w))
-}) 
-
-test_that("range value stops for character vectors", {
-  w <- letters[1:5]
-  
   expect_error(range_value(w, na.rm = TRUE))
 })
+
 
 
 
@@ -85,34 +50,6 @@ source("../functions/missing-values.R")
 # context with one test that groups expectations
 context("Test for missing value") 
 
-
-test_that("test for missing value in the vector", {
-  x <- c(1, 2, 3, 4, 5)
-  expect_length(missing_values(x), 1)
-  expect_type(missing_values(x), 'integer')
-  expect_gte(missing_values(x), 0)
-})
-
-test_that("test for missing value in the vector", {
-  x <- c(1, 2, 3, 4, 5)
-  expect_length(missing_values(x), 1)
-  expect_type(missing_values(x), 'integer')
-  expect_gte(missing_values(x), 0)
-})
-
-test_that("test for missing value in the vector", {
-  x <- c(1, 2, 3, 4, 5)
-  expect_length(missing_values(x), 1)
-  expect_type(missing_values(x), 'integer')
-  expect_gte(missing_values(x), 0)
-})
-
-test_that("test for missing value in the vector", {
-  x <- c(1, 2, 3, 4, 5)
-  expect_length(missing_values(x), 1)
-  expect_type(missing_values(x), 'integer')
-  expect_gte(missing_values(x), 0)
-})
 
 test_that("missing value", {
   x <- c(1, 2, 3, 4, 5)
